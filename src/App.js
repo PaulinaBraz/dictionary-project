c70004f7bf63838c14cd3aebe33c7a19e4775d54
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-05-01 19:11:23 +0100

    Added h1 in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello!!!</h1>
         <button className="btn btn-primary">Click</button>
       </header>
     </div>
