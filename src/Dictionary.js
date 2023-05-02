a301eca78195c08a04c49f9a24b030e98ab7951b
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-05-02 08:41:00 +0100

    Added API integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyWord, setKeyWord] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert("Searching for " + keyWord + " definiction");
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
+    axios.get(apiUrl).then(handleResponse);
   }
+
   function handleChange(event) {
     setKeyWord(event.target.value);
   }
