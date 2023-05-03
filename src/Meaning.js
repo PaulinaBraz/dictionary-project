0004d049bba5c962921876e5dc3d9eddbe827e03
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-05-03 08:50:27 +0100

    changes in meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,12 +4,11 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeach}</h3>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
               <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
