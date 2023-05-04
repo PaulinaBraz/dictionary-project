5c161a04e4fdd79914c3372f75577f2f32c656b3
PaulinaBraz <124574292+PaulinaBraz@users.noreply.github.com>
2023-05-04 10:25:15 +0100

    CSS color changes

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -10,7 +10,7 @@ export default function Dictionary(props) {
   let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
-  function handleDictionResponse(response) {
+  function handleDictionary(response) {
     setResults(response.data[0]);
   }
 
@@ -19,14 +19,11 @@ export default function Dictionary(props) {
   }
 
   function search() {
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleDictionResponse);
-
-    let pexelsApiKey =
-      "PsLTG0vUousYgmUSrLTIEhpbXxFLrmgGCgtMnQ5IJyLonQCk9h8TvIuw";
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
-    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleDictionary);
+    let sheCodesApiKey = "bd79ao40tde3dec118ca46bc3e6dd55f";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey} `;
+    axios.get(sheCodesApiUrl).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
